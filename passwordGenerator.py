2c532ae4761c1d01115b60b50a7c36bd65c932b7
Jephy <jephy.ntutu@gmail.com>
2025-05-08 12:02:49 -0400

    apply changes increasing clarity and readability

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -8,7 +8,6 @@ Choose option:
 1: generate password
 2: exit the program
 Your choice: '''
-choice = ''
 
 
 def pick(sequence):
@@ -23,56 +22,62 @@ def print_error():
     print('Please enter a correct value')
 
 
-def interact_main_menu():
-    global choice
+def add_valid_option(password_options: list, input_option, name):
+    if input_option in ('y', 'Y'):
+        add_element(password_options, name)
+    elif input_option not in ('n', 'N'):
+        print_error()
+        return False
+    return True
+
+
+def interact_main_menu(choice=''):
     while choice != '2':
         choice = input(menu)
         if choice == '1':
-            options = [0]
+            options = ["lowercase"]
             try:
                 length = int(input('Provide password length:'))
+                if length < 1:
+                    print_error()
+                    continue
             except ValueError:
                 print_error()
                 continue
             uppercase = input('Use uppercase letters? (y/n):').strip()
-            if uppercase in ('y', 'Y'):
-                add_element(options, 1)
-            elif uppercase not in ('n', 'N'):
-                print_error()
+            if not add_valid_option(options, uppercase, "uppercase"):
                 continue
             digit = input('Use digits? (y/n):').strip()
-            if digit in ('y', 'Y'):
-                add_element(options, 2)
-            elif digit not in ('n', 'N'):
-                print_error()
+            if not add_valid_option(options, digit, "digit"):
                 continue
             special_character = input('Use special characters? (y/n):').strip()
-            if special_character in ('y', 'Y'):
-                add_element(options, 3)
-            elif special_character not in ('n', 'N'):
-                print_error()
+            if not add_valid_option(options, special_character, "special_character"):
                 continue
-            characters = []
-            for i in range(length):
-                option = pick(options)
 
-                match option:
-                    case 0:
-                        add_element(characters, pick(string.ascii_lowercase))
-                    case 1:
-                        add_element(characters, pick(string.ascii_uppercase))
-                    case 2:
-                        add_element(characters, pick(string.digits))
-                    case 3:
-                        add_element(characters, pick('!@#$%&^|()_+'))
-
-            password = ''.join(characters)
+            password = generate_password(length, options)
             print('Generate password: ', password)
         elif choice == '2':
             print('Bye!')
             sys.exit()
         else:
             print_error()
+
+
+def generate_password(length, options):
+    characters = []
+    for i in range(length):
+        option = pick(options)
+        match option:
+            case "lowercase":
+                add_element(characters, pick(string.ascii_lowercase))
+            case "uppercase":
+                add_element(characters, pick(string.ascii_uppercase))
+            case "digit":
+                add_element(characters, pick(string.digits))
+            case "special_character":
+                add_element(characters, pick('!@#$%&^|()_+'))
+    password = ''.join(characters)
+    return password
 
 
 if __name__ == '__main__':
